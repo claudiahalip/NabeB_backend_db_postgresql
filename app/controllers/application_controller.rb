a6ab96cfae8d0531c089fd9ab4f6d201ff39c044
claudiahalip <claudia4ro@yahoo.com>
2020-12-18 23:34:54 -0600

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
         !!session[:user_id]
     end
 
-    def curent_user
+    def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
