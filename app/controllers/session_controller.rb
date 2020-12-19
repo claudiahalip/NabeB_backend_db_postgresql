0098b01818da779ec10dfd6838e47adc78633928
claudiahalip <claudia4ro@yahoo.com>
2020-12-18 23:47:47 -0600

    fixed typo in session controler

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
 
     def create
         @user = User.find_by(email: session_params[:email])
-        if @user && @user.authenticate(sesion_params[:password])
+        if @user && @user.authenticate(session_params[:password])
             login!
             render json: {logged_in: true, user: @user}
         else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
   get '/logged_in', to: 'session#is_logged_in?'
 
