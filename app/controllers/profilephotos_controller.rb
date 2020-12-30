d5a67f18b81276e711beaf1ecb85ce029bd0a39a
claudiahalip <claudia4ro@yahoo.com>
2020-12-29 19:22:05 -0600

    index, create, show, destroy actions set

--- app/controllers/profilephotos_controller.rb
+++ app/controllers/profilephotos_controller.rb
@@ -7,13 +7,33 @@ class ProfilephotosController < ApplicationController
     end
 
     def create
+        photo = Cloudinary::Uploader.upload(params[:image])
+        profilephoto = Profilephoto.create({image: photo, user_id: profilephoto_params[:user_id]})
+        if profilephoto.save 
+            render json: {profilephoto: profilephoto}
+        else 
+            render json: {status: 500, errors: @user.errors.full_messages}
+        end
     end
 
     def show
-        
+        profilephoto = Profilephoto.find(profilephotos_params[:id])
+        if profilephoto
+            render json: {profilephoto: profilephoto}
+        else
+            render json: {status: 500, errors: ['profile photo not found']}
+        end
     end 
      
     def destroy
+        profilephoto = Profilephoto.find(profilephotos_params[:id])
+        profilephoto.clear
 
     end 
+
+    private
+
+    def profilephotos_params
+       params.require(:profilephoto).require(:image, :user_id)
+    end
 end 
